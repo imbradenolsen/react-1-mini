8a9864d00f2b87970a744927513448d32c6e15f5
Braden Olsen <bradenolsen12@gmail.com>
2017-12-01 12:55:31 -0700

    All done

--- src/App.js
+++ src/App.js
@@ -24,6 +24,17 @@ class App extends Component {
     })
   }
 
+  addFriend(){
+    this.setState({
+      picture: '',
+      name: '',
+      friends: [...this.state.friends, {
+        picture: this.state.picture,
+        name: this.state.name,
+      }]
+    })
+  }
+
 
   render() {
     return (
@@ -34,7 +45,15 @@ class App extends Component {
         Name:
         <input onChange ={event => this.updateName(event.target.value)} value={this.state.name}/>
 
-        <button>Add Friend</button>
+        <button onClick={() => this.addFriend() }>Add Friend</button>
+          <div>
+            {this.state.friends.map(friend => (
+              <div>
+                <img src={friend.picture} width ="100px" />
+                Name: {friend.name}
+              </div>
+            ))}
+          </div>
 
         <div>
           State: {JSON.stringify(this.state)}
